fe6a766985825dc3e86d2174322e1f2b3b4fe5a9
Marcio Melo <marciondemelo@gmail.com>
2021-07-06 11:47:25 -0300

    adicionado jpa e banco de dados h2

--- src/main/java/com/mm/curso/entiities/User.java
+++ src/main/java/com/mm/curso/entiities/User.java
@@ -4,12 +4,20 @@ package com.mm.curso.entiities;
 import lombok.Setter;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Getter
 @Setter
+@Entity
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
